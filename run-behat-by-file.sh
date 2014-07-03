97655e4af9e2b6ab92f98349afa1ac2643a83d20
gperr1 <gperr1@users.noreply.github.com>
2014-07-03 18:06:23 +0200

    Update run-behat-by-file.sh

--- run-behat-by-file.sh
+++ run-behat-by-file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull origin master
 sed "s/@base_url@/$1/" $PWD/src/tests/behat.yml-dist > $PWD/src/tests/behat.yml
-ant behat.run -Dbehat.test Dbehat.feature="$2"
+ant behat.test -Dbehat.feature="$2"
